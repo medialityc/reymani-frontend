0dd9b7c9ccbb0dad886aef9bf3432037ad5304d1
rafaelrdgz <rafaelrdgz2003@gmail.com>
2025-01-01 16:24:56 -0500

    feat(role): added delete rol

--- src/services/RolService.ts
+++ src/services/RolService.ts
@@ -89,3 +89,19 @@ export const getRolePermissions = async (id: string): Promise<PermisoDto[]> => {
     return []
   }
 }
+
+export const deleteRol = async (id: string): Promise<void> => {
+  try {
+    await api.delete(`/rol/${id}`)
+  } catch (error: any) {
+    if (error.response && error.response.status === 401) {
+      throw error
+    }
+
+    if (error.response && error.response.status === 404) {
+      throw new Error('Rol no encontrado')
+    }
+
+    throw new Error('Error eliminando rol')
+  }
+}
--- src/views/roles/RolesTable.tsx
+++ src/views/roles/RolesTable.tsx
@@ -8,15 +8,45 @@ import type { GridColDef } from '@mui/x-data-grid'
 import { DataGrid, GridActionsCellItem, GridToolbar } from '@mui/x-data-grid'
 import { esES } from '@mui/x-data-grid/locales'
 
-import { fetchRoles } from '@/services/RolService'
+import { enqueueSnackbar, SnackbarProvider } from 'notistack'
+
+import { fetchRoles, deleteRol } from '@/services/RolService'
 import type { RolDto } from '@/types/dtos/RolDto'
 import usePermissions from '@/hooks/usePermissions'
+import ConfirmationDialog from '@/components/ConfirmationDialog'
 
 export default function RolesTable() {
   const [rows, setRows] = useState<RolDto[]>([])
   const [loading, setLoading] = useState(true)
   const router = useRouter()
   const { hasPermission } = usePermissions()
+
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const handleDialogClose = () => setDialogOpen(false)
+  const [selectedId, setSelectedId] = React.useState('')
+
+  const handleDeleteClick = (id: any) => () => {
+    setDialogOpen(true)
+    setSelectedId(id)
+  }
+
+  const handleDialogAgree = async () => {
+    setDialogOpen(false)
+
+    try {
+      await deleteRol(selectedId)
+      setRows(rows.filter(row => row.id !== selectedId))
+      enqueueSnackbar('Rol eliminado correctamente', { variant: 'success' })
+    } catch (error: any) {
+      if (error.response && error.response.status === 404) {
+        enqueueSnackbar('Rol no encontrado', { variant: 'error' })
+      } else if (error.response && error.response.status === 401) {
+        router.push('/unauthorized')
+      } else {
+        enqueueSnackbar('Error eliminando rol', { variant: 'error' })
+      }
+    }
+  }
 
   const columns: GridColDef[] = [
     //{ field: 'idRol', headerName: 'ID', flex: 1 },
@@ -30,16 +60,30 @@ export default function RolesTable() {
       cellClassName: 'actions',
       getActions: ({ id }) => {
         if (hasPermission('Actualizar_Rol')) {
-          return [
+          const actions = [
             // eslint-disable-next-line react/jsx-key
             <GridActionsCellItem
-              icon={<i className='ri-edit-2-fill' />}
+              icon={<i className='ri-edit-2-line' />}
               label='Editar'
               className='textPrimary'
               onClick={() => router.push(`/roles/${id}/edit`)}
               color='inherit'
             />
           ]
+
+          if (hasPermission('Eliminar_Rol')) {
+            actions.push(
+              // eslint-disable-next-line react/jsx-key
+              <GridActionsCellItem
+                icon={<i className='ri-delete-bin-7-line' />}
+                onClick={handleDeleteClick(id)}
+                label='Eliminar'
+                color='inherit'
+              />
+            )
+          }
+
+          return actions
         }
 
         return []
@@ -47,26 +91,27 @@ export default function RolesTable() {
     }
   ]
 
-  useEffect(() => {
-    const getRoles = async () => {
-      try {
-        const data = await fetchRoles()
+  async function getRoles() {
+    try {
+      const data = await fetchRoles()
 
-        setRows(data)
-      } catch (error: any) {
-        if (error.response && error.response.status === 401) {
-          router.push('/unauthorized')
-        }
-      } finally {
-        setLoading(false)
+      setRows(data)
+    } catch (error: any) {
+      if (error.response && error.response.status === 401) {
+        router.push('/unauthorized')
       }
+    } finally {
+      setLoading(false)
     }
+  }
 
+  useEffect(() => {
     getRoles()
   }, [])
 
   return (
     <div style={{ height: 650, width: '100%' }}>
+      <SnackbarProvider maxSnack={3} />
       <DataGrid
         localeText={esES.components.MuiDataGrid.defaultProps.localeText}
         sx={{ height: 650, width: '100%', marginTop: 5, backgroundColor: 'var(--mui-palette-background-paper)' }}
@@ -79,6 +124,13 @@ export default function RolesTable() {
         slots={{ toolbar: GridToolbar }}
         rows={rows}
         columns={columns}
+      />
+      <ConfirmationDialog
+        title={'Eliminar Rol'}
+        text={'¿Está seguro que desea eliminar este rol?'}
+        open={dialogOpen}
+        handleClose={handleDialogClose}
+        handleAgree={handleDialogAgree}
       />
     </div>
   )

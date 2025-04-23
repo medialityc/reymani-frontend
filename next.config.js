3cf84e2e993a5568e7c4f1cd0102ab96553bf25d
Kristian Carreño <kristiancarreno02@gmail.com>
2025-04-22 21:28:15 -0400

    ci/cd: add caprover deploy

--- next.config.js
+++ next.config.js
@@ -12,5 +12,6 @@ module.exports = {
         hostname: imageServerHostname
       }
     ]
-  }
+  },
+  output: 'standalone'
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.BASEPATH
+  basePath: process.env.BASEPATH,
+  output: 'standalone'
 }
 
 export default nextConfig

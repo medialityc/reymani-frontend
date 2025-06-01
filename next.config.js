b19c4fc9411be0600058ea6b06702d6d0b6ca3be
Roger Oliva <rogeroliva44@gmail.com>
2025-05-31 23:15:08 -0400

    conlficto resuelto
    conflicto resuelto

--- next.config.js
+++ next.config.js
@@ -6,10 +6,7 @@ const imageServerHostname = imageServer.replace(/^https?:\/\//, '')
 
 module.exports = {
   images: {
-<<<<<<< Updated upstream
-=======
     unoptimized: true,
->>>>>>> Stashed changes
     remotePatterns: [
       {
         protocol: imageServer.startsWith('https') ? 'https' : 'http',
